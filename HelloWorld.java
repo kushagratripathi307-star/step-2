d054d273c16cce7915964e3d837302b97a3cf786
kushagratripathi307-star <kushagra.tripathi307@gmail.com>
2026-03-26 15:03:07 +0530

    Implement UC4: Display Hello with Multiple Command-Line Arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // UC3: Display Hello with Command-Line Argument or Default Message
+        // UC4: Display Hello with Multiple Command-Line Arguments or Default Message
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            System.out.println("Hello " + String.join(" ", args));
         } else {
             System.out.println("Hello World");
         }
